62dddac3566274314385872a825d1336a236bdfb
Ranjeet528 <Ranjeetgurjar528@gmail.com>
2026-07-03 17:16:15 +0530

    fix auth middleware and cart sync routes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const  cors = require("cors");
 let cookieParser = require('cookie-parser')
 const app = express();
-app.use(express.json());
+app.use(express.json());    
 app.use(cors({
     origin: "http://localhost:3000",
     credentials: true
