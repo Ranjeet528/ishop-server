46fb55d7ebe4732a1a847115cfd994fdf9ca32ed
Ranjeet528 <Ranjeetgurjar528@gmail.com>
2026-07-05 00:56:27 +0530

    fix cors for vercel frontend

--- server.js
+++ server.js
@@ -6,8 +6,11 @@ let cookieParser = require('cookie-parser')
 const app = express();
 app.use(express.json());    
 app.use(cors({
-    origin: "http://localhost:3000",
-    credentials: true
+  origin: [
+    "http://localhost:3000",
+    "https://ishop-client-xi.vercel.app",
+  ],
+  credentials: true,
 }));
 app.use(cookieParser());
 app.use(express.static("./public"));
